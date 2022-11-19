e3f0211b0e1ef989725d3b86ab0ddd3dbae075b0
Valdis <fbadmang1@gmail.com>
2022-11-19 18:57:37 +0200

    First commit

---
+++ src/ArrayStorage.java
@@ -0,0 +1,66 @@
+/**
+ * Array based storage for Resumes
+ */
+public class ArrayStorage {
+    Resume[] storage = new Resume[10000];
+
+    void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
+    }
+
+    void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
+            }
+        }
+    }
+
+    Resume get(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                return storage[i];
+            }
+        }
+        return null;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].toString().equals(uuid)) {
+                storage[i] = null;
+            }
+        }
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                count ++;
+            }
+        }
+
+        Resume[] cleanedResume = new Resume[count];
+
+        for (int i = 0, j = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                cleanedResume[j] = storage[i];
+                j++;
+            }
+        }
+        return cleanedResume;
+    }
+
+    int size() {
+        return 0;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+
+public class Main {
+
+
+    public static void main(String[] args) {
+        ArrayStorage arr1 = new ArrayStorage();
+
+        Resume r1 = new Resume();
+        Resume r2 = new Resume();
+        Resume r3 = new Resume();
+        r1.uuid = "uuid1";
+        r2.uuid = "uuid2";
+        r3.uuid = "uuid3";
+
+        arr1.save(r1);
+        arr1.save(r2);
+        arr1.save(r3);
+
+        System.out.println(arr1.get("uuid1"));
+        arr1.delete("uuid1");
+        System.out.println(Arrays.toString(arr1.storage));
+
+
+        System.out.println(Arrays.toString(arr1.getAll()));
+
+
+        String[] intArr = new String[5];
+        intArr[0] = "str1";
+        intArr[1] = "str2";
+
+
+
+
+    }
+}
